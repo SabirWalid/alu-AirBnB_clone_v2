89ea5b19391ddbc89bf03ac75be772d5c432c2e1
SabirWalid <s.abdurahma@alustudent.com>
2023-10-19 13:17:43 +0200

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -16,10 +16,12 @@ def hello_hbnb():
     """Display "Hello HBNB!"."""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """Display "HBNB"."""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
